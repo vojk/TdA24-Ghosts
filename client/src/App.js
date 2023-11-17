cbf7ba442a95db8e25cc079397ea987655d517d3
ondra-pog <ondra.bohm05@seznam.cz>
2023-11-17 19:45:30 +0100

    Stránka /lecturer už má dummy vizitku, na /lecturer/{uuid} už můžu začít dělat dynamický vizitky. Přidána 404 stránka.

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,30 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "./App.css"
-import { useEffect, useState } from 'react';
+import { ListVizitek } from "./components/ListVizitek";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import ErrorPage from "./components/error";
 
-function App() {
-  return (<ListVizitek />)
-}
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-function Vizitka({ lecturerData }) {
-  const name = lecturerData.first_name;
-  const mid_name = lecturerData.middle_name;
-  const surname = lecturerData.last_name;
-  const title_b = lecturerData.title_before;
-  const title_a = lecturerData.title_after;
-  const pic_url = lecturerData.picture_url;
-  const cena = lecturerData.price_per_hour;
-  const tagy = lecturerData.tags;
-  const location = lecturerData.location;
-  const claim = lecturerData.claim;
-  const bio = lecturerData.bio;
-  const telephone_numbers = lecturerData.contact.telephone_numbers;
-  const emails = lecturerData.contact.emails;
+function Profil() {
+  const { UUID } = useParams();
+
   return (
-    <div key={lecturerData.UUID} className='vizitka max-w-sm p-4 overflow-hidden bg-sky rounded-lg'>
-      <img src={pic_url} alt={'Picture of ' + name} className="rounded-full h-55 m-auto" />
-
-      <h1 className='text-4xl text-center text-prussian font-nadpis p-2'>{title_b} <br /> {name} {mid_name} {surname} <br /> {title_a}</h1>
-
-      <h2 className="text-xl text-center">Působí v: <br /> <span className="text-3xl text-sunglow font-nadpis">{location}</span></h2>
-      <h2 className="text-xl text-center">Cena: <br /> <span className="text-3xl text-sunglow font-nadpis">{cena}</span></h2>
-      <h2 className='text-md italic text-prussian text-center'>{claim}</h2>
-      <div className="shade-parent">
-
-        <div dangerouslySetInnerHTML={{ __html: bio }} className='bio text-md font-semibold' /> {/* bude na spodu v rozbalovacím boxu? nebo proklik na samostatnou stránku. bude se loadit jen max délka textu v náhledu? */}
-        {/* limitovat délku pomocí CSS */}
-        <div className="shade bg-sky" />
-      </div>
-
-      <div className="kontakty flex gap-1 flex-col"> {/* zkoušel jsem tady udělat responzivitu, kde byl mail a telefon vedle sebe na desktopu, ale delší maily zlobí a když se nevejdou, tak je to otřesný, raději nechám samostatný řádku pro všechny obrazovky */}
-        <div>
-          {telephone_numbers.map((num, index) => (
-            <div className="flex w-fit m-auto">
-              <FontAwesomeIcon icon={faPhone} className="my-auto m-1" />
-              <div className='num' key={index}>
-                {num}
-              </div>
-            </div>
-
-          ))}
-        </div>
-        <div>
-          {emails.map((mail, index) => (
-            <div className="flex w-fit m-auto">
-              <FontAwesomeIcon icon={faEnvelope} className="my-auto m-1" />
-              <div className='mail' key={index}> {/* prostě vypíše maily do samostatných divů s classou "mail" */}
-                {mail}
-              </div>
-            </div>
-
-          ))}
-        </div>
-      </div>
-
-      <div className="shade-parent">
-        <div className='tagy h-56 flex flex-wrap '> {/* div okolo všech tagů, ještě idk co s tím bude, třeba nějaký pozadí a stylování */}
-          {tagy.map(tag => (
-            <div key={tag.uuid} className='tag text-jet bg-sunglow m-1 p-1 w-fit max-h-8 rounded-md'> {/* nevim co ten key dělá ale radši ho tam dám, vypíše všechny tagy v samostatným divu s classou "tag" */}
-              {tag.name}
-            </div>
-          ))}
-        </div>
-        <div className="shade bg-sky" />
-      </div>
-
-    </div>
-  );
+    <>
+      <p className="text-jet text-4xl">Data uživatele: {UUID}<br />možná někdy...</p>
+    </>
+  )
 }
 
-function ListVizitek() {
-  const fakeAPI = [{
+function ProfilExample() {
+  const data = { /* tady bude fetch pro konkrétní profil */
     "UUID": "67fda282-2bca-41ef-9caf-039cc5c8dd69",
     "title_before": "Mgr.",
     "first_name": "Petra",
@@ -128,200 +74,107 @@ function ListVizitek() {
       "telephone_numbers": ["+420 722 482 974"],
       "emails": ["placha@scg.cz", "predseda@scg.cz"]
     }
-  },
-  {
-    "UUID": "2d7e0a53-5a47-4b05-b8ea-60b3d7c8257c",
-    "title_before": "Dr.",
-    "first_name": "John",
-    "middle_name": "Alexander",
-    "last_name": "Smith",
-    "title_after": "PhD",
-    "picture_url": "https://picsum.photos/300",
-    "location": "New York",
-    "claim": "Medical Doctor",
-    "bio": "<p>I'm a passionate medical doctor with years of experience.</p>",
-    "tags": [
-      {
-        "uuid": "a1",
-        "name": "Medicine"
-      },
-      {
-        "uuid": "a2",
-        "name": "Healthcare"
-      }
-    ],
-    "price_per_hour": 1500,
-    "contact": {
-      "telephone_numbers": ["+1 123-456-7890"],
-      "emails": ["john.smith@example.com"]
-    }
-  },
-  {
-    "UUID": "1e27c527-4f8b-4b12-9c4a-73a29b61de31",
-    "title_before": "Mr.",
-    "first_name": "David",
-    "middle_name": "James",
-    "last_name": "Johnson",
-    "title_after": "MSc",
-    "picture_url": "https://picsum.photos/300",
-    "location": "Los Angeles",
-    "claim": "Software Engineer",
-    "bio": "<p>I'm a software engineer with a passion for coding.</p>",
-    "tags": [
-      {
-        "uuid": "b1",
-        "name": "Programming"
-      },
-      {
-        "uuid": "b2",
-        "name": "Web Development"
-      }
-    ],
-    "price_per_hour": 1000,
-    "contact": {
-      "telephone_numbers": ["+1 987-654-3210"],
-      "emails": ["david.johnson@example.com"]
-    }
-  },
-  {
-    "UUID": "3b96e468-7d12-4f63-a4de-89a7c9ff7a45",
-    "title_before": "Mrs.",
-    "first_name": "Emily",
-    "middle_name": "Grace",
-    "last_name": "Brown",
-    "title_after": "MA",
-    "picture_url": "https://picsum.photos/300",
-    "location": "Chicago",
-    "claim": "Marketing Manager",
-    "bio": "<p>I'm a marketing professional with a knack for strategy.</p>",
-    "tags": [
-      {
-        "uuid": "c1",
-        "name": "Marketing"
-      },
-      {
-        "uuid": "c2",
-        "name": "Brand Management"
-      }
-    ],
-    "price_per_hour": 1200,
-    "contact": {
-      "telephone_numbers": ["+1 555-123-4567"],
-      "emails": ["emily.brown@example.com"]
-    }
-  },
-  {
-    "UUID": "4f76a1ea-d0a3-4e36-bc86-9467ae5aee1c",
-    "title_before": "Miss",
-    "first_name": "Sophia",
-    "middle_name": "Lynn",
-    "last_name": "Garcia",
-    "title_after": "BA",
-    "picture_url": "https://picsum.photos/300",
-    "location": "San Francisco",
-    "claim": "Graphic Designer",
-    "bio": "<p>I'm a creative graphic designer with a love for visual arts.</p>",
-    "tags": [
-      {
-        "uuid": "d1",
-        "name": "Graphic Design"
-      },
-      {
-        "uuid": "d2",
-        "name": "Illustration"
-      }
-    ],
-    "price_per_hour": 800,
-    "contact": {
-      "telephone_numbers": ["+1 555-987-6543"],
-      "emails": ["sophia.garcia@example.com"]
-    }
-  },
-  {
-    "UUID": "5c89f3f5-8d2b-487e-98f9-ace139a5a3ab",
-    "title_before": "Mr.",
-    "first_name": "Michael",
-    "middle_name": "Andrew",
-    "last_name": "Williams",
-    "title_after": "JD",
-    "picture_url": "https://picsum.photos/300",
-    "location": "Houston",
-    "claim": "Lawyer",
-    "bio": "<p>I'm an experienced lawyer with expertise in various legal matters.</p>",
-    "tags": [
-      {
-        "uuid": "e1",
-        "name": "Legal Consultation"
-      },
-      {
-        "uuid": "e2",
-        "name": "Litigation"
-      }
-    ],
-    "price_per_hour": 2000,
-    "contact": {
-      "telephone_numbers": ["+1 888-123-4567"],
-      "emails": ["michael.williams@example.com"]
-    }
-  },
-  {
-    "UUID": "6abca11b-ee18-4b20-8a1b-5f0a15e7ed0b",
-    "title_before": "Mrs.",
-    "first_name": "Olivia",
-    "middle_name": "Ann",
-    "last_name": "Martinez",
-    "title_after": "PharmD",
-    "picture_url": "https://picsum.photos/300",
-    "location": "Miami",
-    "claim": "Pharmacist",
-    "bio": "<p>I'm a licensed pharmacist with expertise in medication management.</p>",
-    "tags": [
-      {
-        "uuid": "f1",
-        "name": "Pharmacy"
-      },
-      {
-        "uuid": "f2",
-        "name": "Medication Counseling"
-      }
-    ],
-    "price_per_hour": 1500,
-    "contact": {
-      "telephone_numbers": ["+1 786-555-7890"],
-      "emails": ["olivia.martinez@example.com"]
-    }
-  }]
+  }
+  const name = data.first_name;
+  const mid_name = data.middle_name;
+  const surname = data.last_name;
+  const title_b = data.title_before;
+  const title_a = data.title_after;
+  const pic_url = data.picture_url;
+  const cena = data.price_per_hour;
+  const tagy = data.tags;
+  const location = data.location;
+  const claim = data.claim;
+  const bio = data.bio;
+  const telephone_numbers = data.contact.telephone_numbers;
+  const emails = data.contact.emails;
+  return (
+    <div key={data.UUID} className='vizitka max-w-fit p-5 overflow-hidden bg-sky rounded-lg grid grid-cols-2 grid-rows-2 m-5'>
+      <div className="col-span-1">
+        <img src={pic_url} alt={'Picture of ' + name} className="rounded-full h-55 m-auto" />
+        <h1 className='text-4xl text-center text-prussian font-nadpis p-2'>{title_b} <br /> {name} {mid_name} {surname} <br /> {title_a}</h1>
+      </div>
 
-  const [data, setData] = useState([]);
-  const [records, setRecords] = useState(data);
+      <div className="col-span-1">
+        <h2 className="text-xl text-center"><span>Působí v: </span><span className="text-3xl text-sunglow font-nadpis">{location}</span></h2>
+        <h2 className="text-xl text-center"><span>Cena: </span><span className="text-3xl text-sunglow font-nadpis">{cena}</span></h2>
+        <h2 className='text-md italic text-prussian text-center'>{claim}</h2>
+        
 
-  useEffect(() => { /* tady bude fetch nebo tak něco idk */
-    setData(fakeAPI);
-    setRecords(fakeAPI);
-  }, [])
+          <div dangerouslySetInnerHTML={{ __html: bio }} className='text-xl' /> {/* bude na spodu v rozbalovacím boxu? nebo proklik na samostatnou stránku. bude se loadit jen max délka textu v náhledu? */}
+          {/* limitovat délku pomocí CSS */}
+          
+        
+      </div>
 
 
-  /* pro checkboxy tohle -> https://medium.com/@compmonk/react-multi-select-with-check-boxes-and-select-all-option-bd16941538f3 */
+     {/*  <div className=""> */}
+        <div className="kontakty flex gap-1 flex-col col-span-1"> {/* zkoušel jsem tady udělat responzivitu, kde byl mail a telefon vedle sebe na desktopu, ale delší maily zlobí a když se nevejdou, tak je to otřesný, raději nechám samostatný řádku pro všechny obrazovky */}
+          <div>
+            {telephone_numbers.map((num, index) => (
+              <div className="flex w-fit m-auto">
+                <FontAwesomeIcon icon={faPhone} className="my-auto m-2" />
+                <div className='num' key={index}>
+                  {num}
+                </div>
+              </div>
 
-  const Filter = (event) => {
-      console.log(event)
-      setRecords(data.filter(f =>
-        f.location.toLowerCase().includes(event.target.value.toLowerCase())
-        /* nastaví "records" na data pokud obsahují hodnotu xy */
-  ))}
+            ))}
+          </div>
+          <div>
+            {emails.map((mail, index) => (
+              <div className="flex w-fit m-auto">
+                <FontAwesomeIcon icon={faEnvelope} className="my-auto m-1" />
+                <div className='mail' key={index}> {/* prostě vypíše maily do samostatných divů s classou "mail" */}
+                  {mail}
+                </div>
+              </div>
 
+            ))}
+          </div>
+        </div>
+        
+          <div className='tagy h-56 flex flex-wrap col-span-1'> {/* div okolo všech tagů, ještě idk co s tím bude, třeba nějaký pozadí a stylování */}
+            {tagy.map(tag => (
+              <div key={tag.uuid} className='tag text-jet bg-sunglow m-1 p-1 w-fit rounded-md'> {/* nevim co ten key dělá ale radši ho tam dám, vypíše všechny tagy v samostatným divu s classou "tag" */}
+                {tag.name}
+              </div>
+            ))}
+          
+          
+        </div>
+     {/*  </div> */}
+    </div>
+  );
+}
+
+function App() {
   return (
     <>
-      <input type="text" onChange={Filter} className="text-jet border-jet w-80" placeholder="filtr měst, později bude checkbox"></input>
-      <input type="checkbox" name="test" value="brno" onChange={Filter}></input>
-      <input type="checkbox" name="test" value="new york" onChange={Filter}></input>
-      <div className='flex flex-wrap gap-3 p-2 justify-center'>
-        {records.map((data) => (
-          <Vizitka key={data.UUID} lecturerData={data} />))}
-      </div>
+      <Router>
+
+        <Link to="/home" className="text-jet m-2 bg-sky p-1 text-center rounded-md" >Home</Link>
+
+        <Link to="/lecturer" className="text-jet m-2 bg-sky p-1 text-center rounded-md">lecturer</Link>
+
+
+
+        <Routes>
+          <Route path="/home" element={<ListVizitek />} ></Route>
+          <Route path="/" element={<ListVizitek />}></Route>
+          <Route path="/lecturer" element={<ProfilExample />} ></Route>
+          <Route path="/lecturer" >
+            <Route path=":UUID" element={<Profil />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+
+        </Routes>
+
+
+      </Router>
+
     </>
   )
 }
-
 
 export default App;
