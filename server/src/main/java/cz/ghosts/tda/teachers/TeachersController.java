7661469115b45c5bc2752e7a09b6c023164d4e35
Vojtěch Tmej <37409766+vojk@users.noreply.github.com>
2024-01-06 15:22:00 +0100

    FixedDB and Enabled CORS

--- server/src/main/java/cz/ghosts/tda/teachers/TeachersController.java
+++ server/src/main/java/cz/ghosts/tda/teachers/TeachersController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/lecturers")
 public class TeachersController {
