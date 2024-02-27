a083381dcc69b1269b6a2e4870b15c0edd027904
ondra-pog <ondra.bohm05@seznam.cz>
2024-02-27 21:09:44 +0100

    Komponenta pro potvrzení rezervace
    Nejsou data k použití a je potřeba přidat filtrování pomocí datumu.
    
    Vyfiltrovaná data se budou mapovat do zmíněného komponentu.

--- client/src/components/LektorAdmin.jsx
+++ client/src/components/LektorAdmin.jsx
@@ -3,16 +3,29 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { useState } from 'react';
 import dayjs from 'dayjs';
+import ReservedUser from './ReservedUser';
 
 
 export default function LektorAdmin() {
-    const [value, setValue] = useState(dayjs());
-    console.log(value.$D,value.$M,value.$y)
+    const [date, setDate] = useState(dayjs());
+    const tomorrow = dayjs().add(1, 'day');
+    console.log(date.$D, date.$M, date.$y)
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
-        </LocalizationProvider>
+        <div className='mt-4 flex max-w-6xl m-auto flex-row md:flex-col-reverse justify-between gap-2 flex-wrap'>
+            <div className='flex items-center flex-col gap-2 flex-1'>
+                <ReservedUser accepted={false}/>
+                <ReservedUser accepted={true}/>
+                <ReservedUser/>
+                <ReservedUser/>
+                <ReservedUser/>
+                <ReservedUser/>
+                <ReservedUser/>
+            </div>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="cs">
+                <DateCalendar maxDate={dayjs().add(12, 'month')} value={date} onChange={(newDate) => setDate(newDate)} className="bg-sky rounded-lg m-0" />
+            </LocalizationProvider>
+        </div>
     )
 }
 
