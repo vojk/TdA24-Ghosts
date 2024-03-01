6ecaec3c9265e9ef44b06fb43c386197c2ff6430
VirtCZ <motlondra@gmail.com>
2024-03-01 17:44:17 +0100

    Rezervace funguje i guess?

---
+++ server/src/main/java/cz/ghosts/tda/reservation/ReservationController.java
@@ -0,0 +1,37 @@
+package cz.ghosts.tda.reservation;
+
+import org.springframework.web.bind.annotation.*;
+
+import cz.ghosts.tda.database.DbReservation;
+
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+  @PostMapping("/{id}")
+  public ResponseEntity<Object> getReservationForTeacher(@PathVariable String id,
+      @RequestBody ReservationDTO entity) {
+    DbReservation dbReservation = new DbReservation();
+    System.out.println(id);
+    entity.setTeacher_id(id);
+    System.out.println("Potvrzeno: " + entity.getSouhlas());
+    dbReservation.createReservation(entity);
+    return ResponseEntity.ok().body(entity);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteReservation(@PathVariable String id) {
+    System.out.println(id);
+    DbReservation dbReservation = new DbReservation();
+    dbReservation.deleteReservation(id);
+    return ResponseEntity.ok().body(id);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getReservation(@PathVariable String id) {
+    DbReservation dbReservation = new DbReservation();
+    return ResponseEntity.ok().body(dbReservation.getReservation(id));
+  }
+
+}
