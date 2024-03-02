a8a4f4b1e541203dbf367fb904f33b35877ff7cc
Vojtěch Tmej <37409766+vojk@users.noreply.github.com>
2024-03-02 13:50:37 +0100

    aaadsf

--- server/src/main/java/cz/ghosts/tda/SecurityConfig.java
+++ server/src/main/java/cz/ghosts/tda/SecurityConfig.java
@@ -14,8 +14,15 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf().disable()
-        .authorizeHttpRequests((request) -> request.requestMatchers(HttpMethod.POST).permitAll().requestMatchers(HttpMethod.GET).permitAll().requestMatchers(HttpMethod.PUT).permitAll().requestMatchers(HttpMethod.DELETE).permitAll().requestMatchers(HttpMethod.POST, "/api/lecturers").hasRole("ADMIN").requestMatchers(HttpMethod.POST, "/api/lecturers/").hasRole("ADMIN"));
+    httpSecurity
+            .authorizeHttpRequests((request) -> request
+                    // Allow unrestricted access to specific paths with different HTTP methods
+                    .requestMatchers(HttpMethod.POST, "/api/reservation/**", "/api/lecturers/a/id_lektor", "/api/credentials/logout", "/api/credentials/login", "/api/reservation").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/**").permitAll()
+                    .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
+                    // Require authentication for POST requests to specific paths
+                    .requestMatchers(HttpMethod.POST, "/api/lecturers", "/api/lecturers/").hasRole("ADMIN"));
 
     return httpSecurity.build();
   }
