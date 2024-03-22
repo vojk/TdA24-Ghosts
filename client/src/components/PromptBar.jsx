1709ff7bc158b3ea8ab8f178e4d9417b937c9d59
ondra-pog <ondra.bohm05@seznam.cz>
2024-03-22 19:01:28 +0100

    url update

--- client/src/components/PromptBar.jsx
+++ client/src/components/PromptBar.jsx
@@ -15,7 +15,7 @@ const [responses, setResponses] = useState([]);
 
             let data = "text: "+text;
 
-            let url = "/api/test"
+            let url = "http://7d17dc13931b9d11.app.tourdeapp.cz/api/test"
             try {
                 const response = await fetch(url, {
                     method: "POST",
