b97c32bbefa2805949298c0e56c0b78ca157e30c
ondra-pog <ondra.bohm05@seznam.cz>
2024-03-22 18:59:48 +0100

    first

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PromptBar from './components/PromptBar';
+import MainChat from './components/MainChat';
+
+function App() {
+  return (
+    <div className="App text-2xl">
+      <MainChat/>
+    </div>
+  );
+}
+
+export default App;
